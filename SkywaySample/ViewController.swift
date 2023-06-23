f8e93f9255e1d7bc5dfcaac1c13c15959ebe13b2
Ryo Tabuchi <tabuchi@as-systems.jp>
2023-06-23 12:06:59 +0900

    Initial Commit

---
+++ SkywaySample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkywaySample
+//
+//  Created by Ryo Tabuchi on 2023/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
